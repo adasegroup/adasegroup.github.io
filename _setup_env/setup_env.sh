29a752687c2f4f0bba71f9e860ccd14374600e0b
Oleg Voinov <oyvoinov@gmail.com>
2020-10-30 16:21:27 +0300

    Downgrade rubygems in setup_env to prevent warnings

--- _setup_env/setup_env.sh
+++ _setup_env/setup_env.sh
@@ -2,7 +2,7 @@
 cd /adasegroup.github.io  &&\
 npm install  &&\
 npm install --global gulp-cli  &&\
-gem update --system  &&\
+gem update --system 3.0.8  &&\
 gem install jekyll bundler  &&\
 bundle install  &&\
 bundle clean --force
